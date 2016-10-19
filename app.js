7a83b7478d782061e3939bc699202b96635f42d5
pipited07 <pipited02@gmail.com>
2016-10-19 19:23:26 +0800

    0

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("hello");
+$(document).ready(function(){
+  $("h1").click(function(){
+    $(this).hide();
+  });
+});
+
+
+$(document).ready(function(){
+	$(".btn1").click(function(){
+	  $("#Box2").hide();
+	  $(this).hide();
+	}); 
+});
